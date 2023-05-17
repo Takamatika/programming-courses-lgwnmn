ee10c9cc7bc4da1f51caea1a6d42a15f941be9a1
Takamatika <132722873+Takamatika@users.noreply.github.com>
2023-05-16 20:42:30 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# programming-courses-lgwnmn
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/programming-courses-lgwnmn)
---
+++ index.ts
@@ -0,0 +1,10 @@
+//######################################################################
+//###########################           SSTech       ###################
+//###########################   Programming Courses  ###################
+//######################################################################
+let myNumber: number;
+myNumber = parseInt(prompt('Write a number <= 3 '));
+while (myNumber > 3) {
+  myNumber = parseInt(prompt('Write a number <= 3 '));
+}
+console.log(myNumber);
